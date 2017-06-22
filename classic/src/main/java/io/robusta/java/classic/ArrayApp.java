a03965721e8b51e36fad066565b3796782389bf7
Max31000 <maxime.desvergne@hotmail.fr>
2017-06-22 08:47:15 +0200

    Wip classic

--- classic/src/main/java/io/robusta/java/classic/ArrayApp.java
+++ classic/src/main/java/io/robusta/java/classic/ArrayApp.java
@@ -1,34 +1,67 @@
 package io.robusta.java.classic;
 
+import java.util.Arrays;
+
 /**
  * Created by nicorama on 21/06/2017.
  */
 public class ArrayApp {
 
-    int[] buildArray(int a, int b, int c){
-        return null;
-    }
 
 
-    boolean equality(int[] array1, int[] array2){
+	int[] buildArray(int a, int b, int c) { // avec a b et c //onrenvoituntableau
 
-        return false;
-    }
+		int[] buildArray = new int[] { a, b, c };
+		return buildArray;
+	}
 
-    String asString (int [] numbers){
-        return null;
-    }
+	public boolean equality(int[] array1, int[] array2){
+		
+//    	int[] Array1= new int[6];
+//    	int[] Array2= new int[8];
+    		if(Arrays.equals(array1, array2)){
+    			return true;
+    			//System.out.println("same");
+    		}
+    			//System.out.println("different");
+    	return false;
+	
+	}
 
-    String asStringJoin (int [] numbers){
-        return null;
-    }
+	
+	String asString(int[] numbers) {
 
-    String asString (String [] strings){
-        return null;
-    }
+		String result="";
+		for (int i=0 ; i< numbers.length; i++){
+			result+= numbers[i]+":";
+		}
+		return result;
+	}
 
-    String asString (Card [] cards){
-        return null;
-    }
+	String asStringJoin(int[] numbers) {
+		String result="";
+		for (int i=0 ; i< numbers.length-1; i++){
+			result+= numbers[i]+":";
+		}result+= numbers[numbers.length-1]+"";
+		return result;
+	}
+	
+	String asString(String[] strings) {
+		String result="";
+		for (int i=0 ; i< strings.length-1; i++){
+			result+= strings[i]+" ";
+		}result+= strings[strings.length-1]+"";
+		return result;
+	}
 
+//	String asString(Card[] cards) {
+//		int value;
+//		char color;
+//		String result ="";
+//		for(int i=0; i< cards.length; i++){
+//			result += cards[i]+"";
+//		}
+//		return String.valueOf(this.value + this.color);
+//	}
+	
 }
